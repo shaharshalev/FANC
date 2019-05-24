5abd26d32961eb181ddecc12c5e529b8baec6e91
shaharshalev <shaharshalev@campus.technion.ac.il>
2019-05-24 21:58:03 +0300

    adding scopes and fixing inheritence

--- fanC.hpp
+++ fanC.hpp
@@ -3,69 +3,71 @@
 
 #include <string>
 #include <vector>
+using namespace std;
 
 class Node{};
 
-class Expression : Node{
+class Expression : public Node{
 public:
     Type type;
 };
 
-class ReturnType : Node{};
-class Void : ReturnType{};
+class ReturnType : public Node{};
+class Void : public ReturnType{};
 
-class Type : ReturnType{ //  TODO: Abstract class
+class Type : public ReturnType{ //  TODO: Abstract class
 };
 
-class IntType : Type{};
+class IntType : public Type{};
 
-class ByteType : Type{};
+class ByteType : public Type{};
 
-class BooleanType : Type{};
+class BooleanType : public Type{};
 
-class BinaryExpression : Expression{
+class BinaryExpression : public Expression{
 public:
     Expression leftExp;
     Expression rightExp;
     Operation op;
 };
 
-class UnaryExpression : Expression{};
+class UnaryExpression : public Expression{};
 
-class Operation : Node{};
+class Operation : public Node{};
 
-class BinaryOperation : Operation{
+class BinaryOperation : public Operation{
 public:
     char op;
 };
 
-class Multiplactive : BinaryOperation{};
+class Multiplactive : public BinaryOperation{};
 
-class Additive : BinaryOperation{};
-class Relop : Operation{};
+class Additive : public BinaryOperation{};
+class Relop : public Operation{};
 
-class BooleanOperation : Operation{};
+class BooleanOperation : public Operation{};
 
-class Boolean : UnaryExpression{};
+class Boolean : public UnaryExpression{};
 
-class ID : UnaryExpression{
+class ID : public UnaryExpression{
 public:
     string name;
+    int offset;
 };
 
-class Call : UnaryExpression{
+class Call : public UnaryExpression{
 public:
     ID id;
     ReturnType returnType;
-    vector<Expression> expressions;
+    vector< Expression > expressions;
 };
 
-class String : UnaryExpression{
+class String : public UnaryExpression{
 public:
     string value;
 };
 
-class Number : UnaryExpression{};
+class Number : public UnaryExpression{};
 
 class FormalDec{
 public:
@@ -73,26 +75,51 @@ public:
     ID id;
 };
 
-class FormalList : Node{
+class FormalList : public Node{
 public:
     vector<FormalDec> declerations;
 };
 
-class PreCondition : Node{
+class PreCondition : public Node{
 public:
     Expression exp;
 };
 
-class PreConditions : Node{
+class PreConditions : public Node{
 public:
     vector<PreCondition> preconditions;
 };
 
-class FuncDec : Node{
+class FuncDec : public Node{
     ReturnType returnType;
     ID id;
     FormalList arguments;
     PreConditions conditions;
+};
+
+class Scope{
+public:
+    vector<ID> ids;
+    virtual void endScope(){}
+
+};
+
+class IfScope: public Scope{
+public:
+    void endScope() override{}
+
+};
+
+class WhileScope: public Scope{
+public:
+    void endScope() override{}
+
+};
+
+class FunctionScope: public Scope{
+public:
+    void endScope() override{}
+
 };
 
 #endif //FANC_H_
